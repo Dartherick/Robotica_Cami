43dfaaded4bd1e09373292e0b428fe367720353a
Dartherick <62523859+Dartherick@users.noreply.github.com>
2023-06-29 06:38:59 -0400

    New HMI in Visual Studio

---
+++ ShooterBot/ShooterBot/CustomButtons.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShooterBot
+{
+    internal class CircularButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0,0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(pevent);
+        }
+    }
+    internal class TriangleButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            Graphics g = pevent.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            float h = this.Height;
+            float w = this.Width;
+
+            PointF[] pts = new PointF[] { new PointF(w / 2, 0), new PointF(0, w), new PointF(w, h) };
+            g.FillPolygon(new SolidBrush(this.BackColor), pts);
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddPolygon(pts);
+
+            this.Region = new Region(gp);
+
+            base.OnPaint(pevent);
+        }
+    }
+}
---
+++ ShooterBot/ShooterBot/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShooterBot
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            button1.Enabled = false;
+
+            if (i == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            button2.Enabled = false;
+
+            if (i == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            button3.Enabled = false;
+
+            if (i == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            button4.Enabled = false;
+
+            if (i == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            button5.Enabled = false;
+
+            if (i == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            button6.Enabled = false;
+
+            if (i == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            i = 0;
+        }
+
+        private void EnableButtons()
+        {
+
+        }
+    }
+}
